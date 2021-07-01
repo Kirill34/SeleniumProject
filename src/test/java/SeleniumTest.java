84e1d54549f268e993587001050dd6370341316e
KKS <kirill.kuliuckin2016@yandex.ru>
2021-07-01 11:34:35 +0300

    First test for Selenium are created

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,29 @@
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+public class SeleniumTest {
+    private static WebDriver driver;
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.gecko.driver", "C:/webdrivers/geckodriver.exe");
+        driver = new FirefoxDriver();
+
+    }
+
+    @Test
+    public void FirstTest() {
+        driver.get("https://www.dns-shop.ru/");
+        Assert.assertTrue(true);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
+}
