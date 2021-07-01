e7a3ffaeedc3de26f239dd51bb164dbd6561483d
KKS <kirill.kuliuckin2016@yandex.ru>
2021-07-01 11:37:08 +0300

    Main function is cleared

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,9 +9,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.gecko.driver", "C:/webdrivers/geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.dns-shop.ru/");
+
 
     }
 }
