78517158cf5c8bcc038878bf4dfdbdcbedafa9eb
KKS <kirill.kuliuckin2016@yandex.ru>
2021-07-01 11:19:06 +0300

    Open DNS website by Selenium

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 public class Main {
-    public void Main()
-    {
-        System.out.print("Hello world");
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "C:/webdrivers/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.dns-shop.ru/");
+
     }
 }
