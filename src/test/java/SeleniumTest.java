2358fee5ba511492156cb7043228c86b348fccf5
KKS <kirill.kuliuckin2016@yandex.ru>
2021-07-20 14:22:14 +0300

    xml file for logging is added

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,7 +1,11 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,11 +13,24 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.concurrent.TimeUnit;
 public class SeleniumTest {
     private static WebDriver driver;
+    private Logger logger = LogManager.getLogger(SeleniumTest.class);
     @BeforeClass
     public static void setup() {
         System.setProperty("webdriver.gecko.driver", "C:/webdrivers/geckodriver.exe");
         driver = DriverFactory.getDriver(DriverType.FIREFOX);
 
+    }
+
+    @BeforeEach
+    public void setUp()
+    {
+        logger.info("Драйвер стартовал!");
+    }
+
+    @AfterEach
+    public void setDown()
+    {
+        logger.info("Драйвер остановлен!");
     }
 
     @Test
